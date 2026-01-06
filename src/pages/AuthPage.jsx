431fee1330a86bd36508aa136bf9f9878033a89c
Austin <austinwong43@gmail.com>
2026-01-06 12:39:40 +0800

    Added Facebook

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -78,6 +78,9 @@ useEffect(() => {
           <Button className="rounded-pill" variant="outline-dark">
             <i className="bi bi-apple"></i> Sign up with Apple
           </Button>
+          <Button className="rounded-pill" variant="outline-dark">
+            <i className="bi bi-facebook"></i> Sign up with FAcebook
+          </Button>
           <p style={{ textAlign: "center" }}>or</p>
           <Button className="rounded-pill" onClick={handleShowSignUp}>
             Create an account
